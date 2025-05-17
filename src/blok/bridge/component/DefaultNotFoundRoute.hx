a2f634d9376bba72afa38f268f185ba04e5285ba
wartman <faxmaster@gmail.com>
2025-05-17 14:37:55 -0500

    formatting

--- src/blok/bridge/component/DefaultNotFoundRoute.hx
+++ src/blok/bridge/component/DefaultNotFoundRoute.hx
@@ -5,9 +5,7 @@ import blok.router.Page;
 
 class DefaultNotFoundRoute extends Page<'*'> {
 	function render():Child {
-		RequestContext.maybeFrom(this).inspect(context -> {
-			context.response.code = NotFound;
-		});
+		RequestContext.maybeFrom(this).inspect(context -> context.response.code = NotFound);
 
 		return DefaultErrorView.node({error: new Error(NotFound, 'Page not found')});
 	}
