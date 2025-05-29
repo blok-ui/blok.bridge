10ff5812b674974552c5c59d22959ef8a39f1c78
wartman <faxmaster@gmail.com>
2025-05-29 08:48:02 -0500

    Add a simple NoHydration component

---
+++ src/blok/bridge/NoHydration.hx
@@ -0,0 +1,26 @@
+package blok.bridge;
+
+import blok.signal.Signal;
+
+class NoHydration extends Component {
+	@:attribute final placeholder:Child = Placeholder.node();
+	@:children @:attribute final child:() -> Child;
+
+	final hydrating:Signal<Bool> = new Signal(false);
+
+	function render() {
+		var env = Root.from(this).adaptor.environment;
+
+		if (env.server) return placeholder;
+
+		hydrating.set(investigate().isHydrating());
+		return if (hydrating()) placeholder else child();
+	}
+
+	function setup() {
+		// This is a bit of a hack: if `hydrating` was false this
+		// will trigger a re-render (as `update` is run *after*
+		// rendering and mounting the component).
+		hydrating.set(false);
+	}
+}
